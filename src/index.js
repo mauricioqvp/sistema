5b9bc46fb835c31865aec77d66333ba885ff8838
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2022-06-03 22:55:15 -0300

    Listing the calls

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+//  <React.StrictMode>
     <App />
-  </React.StrictMode>
+//  </React.StrictMode>
 );
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,34 +1,40 @@
-import { useState, useEffect } from "react"
+
 import './dashboard.css';
+import { useState, useEffect } from 'react';
 
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
+
 import firebase from '../../services/firebaseConnection';
 
-    const listRef = firebase.firestore().collection('chamados').orderBy('created', 'desc');
+const listRef = firebase.firestore().collection('chamados').orderBy('created', 'desc');
 
 export default function Dashboard() {
-
-    const [chamados, setChamados] = useState([1]);
+    const [chamados, setChamados] = useState([]);
     const [loading, setLoading] = useState(true); 
     const [loadingMore, setLoadingMore] = useState(false);
     const [isEmpty, setIsEmpty] = useState(false);
     const [lastDocs, setlastDocs] = useState();
 
+
     useEffect(()=>{
 
         loadChamados();
 
-    },[]);
+        return () => {
+
+        }
+    }, []);
+
 
     async function loadChamados(){
         await listRef.limit(5)
         .get()
         .then((snapshot)=>{
-            updateState(snapshot);
+            updateState(snapshot)
         })
         .catch((err)=>{
             console.log('Deu algum erro: ',err);
@@ -36,7 +42,9 @@ export default function Dashboard() {
         })
 
         setLoading(false);
+
     }
+
 
     async function updateState(snapshot){
         const isCollectionEmpty = snapshot.size=== 0;
@@ -49,7 +57,7 @@ export default function Dashboard() {
                     id: doc.id,
                     assunto: doc.data().assunto,
                     cliente: doc.data().cliente,
-                    clienteIs: doc.data().clienteId,
+                    clienteId: doc.data().clienteId,
                     created: doc.data().created,
                     createdFormated: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
                     status: doc.data().status,
@@ -57,16 +65,47 @@ export default function Dashboard() {
                 })
             })
 
-            const lastDoc = snapshot.docs[snapshot.dosc.length-1]; // Pegando o ultimo documento buscado
+            const lastDoc = snapshot.docs[snapshot.docs.length-1]; // Pegando o ultimo documento buscado
 
             setChamados(chamados => [...chamados, ...lista]);
             setlastDocs(lastDoc);
+            console.log('Chamados: ', chamados);
+            console.log('Lista: ', lista);
         }else{
             setIsEmpty(true);
         }
-
         setLoadingMore(false);
-    };
+    }
+    //console.log(chamados);
+
+
+    async function handleMore(){
+        setLoadingMore(true);
+        await listRef.startAfter(lastDocs).limit(5)
+        .get()
+        .then((snapshot)=>{
+            updateState(snapshot);
+        })
+    }
+
+    if(loading){
+        return(
+            <div>
+                <Header />
+
+                <div className="content">
+                <Title name="Atendimentos">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <div className="container dashboard">
+                    <span>Buscando chamados...</span>
+                </div>
+
+                </div>    
+            </div>
+        )
+    }
 
     return (
         <div>
@@ -81,14 +120,14 @@ export default function Dashboard() {
                     <div className="container dashboard">
                         <span>Nenhum chamado registrado...</span>
 
-                        <Link to="new" className="new" >
+                        <Link to="/new" className="new" >
                             <FiPlus size={25} color="#FFF" />
                             Novo chamado
                         </Link>
                     </div>
                 ) : (
                     <>
-                        <Link to="new" className="new" >
+                        <Link to="/new" className="new" >
                             <FiPlus size={25} color="#FFF" />
                             Novo chamado
                         </Link>
@@ -104,27 +143,33 @@ export default function Dashboard() {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td data-label="Cliente">Sujeito</td>
-                                    <td data-label="Assunto">Suporte</td>
-                                    <td data-label="Status">
-                                        <span className="badge" style={{backgroundColor: '#5cb85c'}}>Em aberto</span>
-                                    </td>
-                                    <td data-label="Cadastrado">20/06/2021</td>
-                                    <td data-label="#">
-                                        <button className="action" style={{backgroundColor: '#3593f6'}}>
-                                            <FiSearch color="#FFF" size={17} />
-                                        </button>
-                                        <button className="action" style={{backgroundColor: '#F6a935'}}>
-                                            <FiEdit2 color="#FFF" size={17} />
-                                        </button>
-                                    </td>
-                                </tr>
+                                {chamados.map((item, index)=>{
+                                    return (
+                                        <tr key={index}>
+                                            <td data-label="Cliente">{item.cliente}</td>
+                                            <td data-label="Assunto">{item.assunto}</td>
+                                            <td data-label="Status">
+                                                <span className="badge" style={{backgroundColor: item.status === 'Aberto' ? '#5cb85c' : '#999'}}>{item.status}</span>
+                                            </td>
+                                            <td data-label="Cadastrado">{item.createdFormated}</td>
+                                            <td data-label="#">
+                                                <button className="action" style={{backgroundColor: '#3593f6'}}>
+                                                    <FiSearch color="#FFF" size={17} />
+                                                </button>
+                                                <button className="action" style={{backgroundColor: '#F6a935'}}>
+                                                    <FiEdit2 color="#FFF" size={17} />
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
                             </tbody>
                         </table>
+                        
+                        {loadingMore && <h3 style={{textAlign: 'center', marginTop: 15 }}>Buscando dados...</h3>}
+                        { !loadingMore && !isEmpty && <button className="btn-more" onClick={handleMore}>Buscar mais</button>}
                     </>
                 )}
-
 
             </div>
 
